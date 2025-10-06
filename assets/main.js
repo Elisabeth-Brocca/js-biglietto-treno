18400203499c668946cd60e00fbecf8f6647f6de
ElisabethBrocca <elisabeth.brocca.07@gmail.com>
2025-10-06 16:08:55 +0200

    [UPD] Prezzo definito in km, fatto

--- assets/main.js
+++ assets/main.js
@@ -8,5 +8,14 @@ numbers / string
 IF / ELSE
 */
 
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere
+let chilometri = prompt('Numero di chilometri')
 
-/* let parola1 = prompt('inserisci una parola') */
+// l'età del passeggero
+let eta = prompt("Età del passeggero")
+
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+let prezzo = chilometri * 0.21
+console.log(prezzo);
+
+// va applicato uno sconto del 20% per i minorenni

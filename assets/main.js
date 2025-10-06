0b75080e0b9a172f8c3957cd5388981f39f2ec73
ElisabethBrocca <elisabeth.brocca.07@gmail.com>
2025-10-06 16:33:09 +0200

    [UPD] applicato sconto 40%

--- assets/main.js
+++ assets/main.js
@@ -17,11 +17,18 @@ let eta = prompt("Età del passeggero")
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 let prezzo = chilometri * 0.21
 
-// va applicato uno sconto del 20% per i minorenni
 if(eta < 18 ){
+    // va applicato uno sconto del 20% per i minorenni
     let prezzoScontato = prezzo - (0.2 * prezzo)
     console.log(prezzoScontato);    
-}else {
-    console.log(prezzo);
+}else if( eta > 65){
+    //va applicato uno sconto del 40% per gli over 65
+    let prezzoScontato = prezzo - (0.4 * prezzo)
+    console.log(prezzoScontato); 
     
+} else {
+    console.log(prezzo);
 }
+
+
+

5a36685480c5e4f75b9ecb9ce32bb0f28181d8d0
ElisabethBrocca <elisabeth.brocca.07@gmail.com>
2025-10-06 15:37:05 +0200

    first commit

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero. 
+
+
+
+TOOLS:
+Variabili let / const
+numbers / string
+IF / ELSE
+*/
+
+
+/* let parola1 = prompt('inserisci una parola') */
